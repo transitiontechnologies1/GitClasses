d38eabdc4d9ab92a8bb6731c91b508b64dbb29c4
lstrobin <lukasz.strobin@gmail.com>
2016-03-15 17:34:57 +0100

    confluct attempt 2

--- src/main/java/pl/com/tt/App.java
+++ src/main/java/pl/com/tt/App.java
@@ -13,7 +13,7 @@ public class App
 NIE ZBUDUJESZ KODU!!!	
     public static void main( String[] args )
     {
-    	logger.info("application started LSTROBIN modification!");
+    	logger.info("conflict indocation LSTROBIN");
         
 		int i = 5;
 		System.out.println(iiaaaaa);
